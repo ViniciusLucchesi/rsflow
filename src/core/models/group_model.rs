440b2b848fb9253c4c4fe4aeedebc091d9b87665
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2024-11-27 21:50:56 -0300

    Adicionar modelo de grupo com validações e testes

---
+++ src/core/models/group_model.rs
@@ -0,0 +1,108 @@
+#[derive(Debug, Clone)]
+pub struct Group {
+    pub id: GroupId,
+    pub name: GroupName,
+    pub description: GroupDescription,
+}
+
+impl Group {
+    pub fn new(name: &str, description: &str) -> Result<Self, &'static str> {
+        let id = GroupId::new();
+        let name = GroupName::new(name)?;
+        let description = GroupDescription::new(description)?;
+
+        Ok(Group { id, name, description })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct GroupId(String);
+
+impl GroupId {
+    pub fn new() -> Self {
+        let uuid = uuid::Uuid::now_v7();
+        let uuid_string = uuid.hyphenated().to_string();
+        GroupId(uuid_string)
+    }
+
+    pub fn value(&self) -> &str {
+        &self.0
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct GroupName(String);
+
+impl GroupName {
+    pub fn new(name: &str) -> Result<Self, &'static str> {
+        if name.trim().is_empty() {
+            Err("Name cannot be empty")
+        } else {
+            Ok(GroupName(name.to_string()))
+        }
+    }
+
+    pub fn value(&self) -> &str {
+        &self.0
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct GroupDescription(String);
+
+impl GroupDescription {
+    pub fn new(description: &str) -> Result<Self, &'static str> {
+        if description.trim().is_empty() {
+            Err("Description cannot be empty")
+        } else {
+            Ok(GroupDescription(description.to_string()))
+        }
+    }
+
+    pub fn value(&self) -> &str {
+        &self.0
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_group_id() {
+        let id = GroupId::new();
+        assert_eq!(id.value().len(), 36);
+    }
+
+    #[test]
+    fn test_group_name() {
+        let name = GroupName::new("Group 1").unwrap();
+        assert_eq!(name.value(), "Group 1");
+    }
+
+    #[test]
+    fn test_group_name_empty() {
+        let name = GroupName::new("");
+        assert!(name.is_err());
+    }    
+
+    #[test]
+    fn test_group_description() {
+        let description = GroupDescription::new("Description 1").unwrap();
+        assert_eq!(description.value(), "Description 1");
+    }
+
+    #[test]
+    fn test_group_description_empty() {
+        let description = GroupDescription::new("");
+        assert!(description.is_err());
+    }
+
+    #[test]
+    fn test_group() {
+        let group = Group::new("Group 1", "Description 1").unwrap();
+        assert_eq!(group.name.value(), "Group 1");
+        assert_eq!(group.description.value(), "Description 1");
+    }
+}
