537a615f65be06232b26cb95b4748bc90380a0df
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2024-11-27 21:50:25 -0300

    Adicionar repositório em memória para grupos e atualizar mensagens de erro no repositório de usuários

--- src/adapters/repositories/in_memory_repository.rs
+++ src/adapters/repositories/in_memory_repository.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 use crate::core::models::user_model::User;
+use crate::core::models::group_model::Group;
 use crate::core::interfaces::database::DatabaseError;
 use crate::core::interfaces::database::user::UserRepository;
+use crate::core::interfaces::database::group::GroupRepository;
+
 
 #[derive(Debug, Clone)]
 pub struct InMemoryUserRepository {
@@ -27,7 +30,7 @@ impl UserRepository for InMemoryUserRepository {
             let user = user_lock.read().map_err(|_| DatabaseError::ReadLockError)?;
             Ok(user.clone())
         } else {
-            Err(DatabaseError::UserNotFound)
+            Err(DatabaseError::NotFoundError)
         }
     }
 
@@ -42,7 +45,7 @@ impl UserRepository for InMemoryUserRepository {
                 return Ok(user.clone());
             }
         }
-        Err(DatabaseError::UserNotFound)
+        Err(DatabaseError::NotFoundError)
     }
 
     fn get_all_users(&self) -> Result<Vec<User>, DatabaseError> {
@@ -60,7 +63,7 @@ impl UserRepository for InMemoryUserRepository {
         let mut data = self.data.write().map_err(|_| DatabaseError::WriteLockError)?;
         
         if data.contains_key(&user.id.value().to_string()) {
-            return Err(DatabaseError::UserAlreadyExists);
+            return Err(DatabaseError::RegisterAlreadyExists);
         }
 
         // Insert the new user with an Arc<RwLock<User>>
@@ -78,7 +81,7 @@ impl UserRepository for InMemoryUserRepository {
             *user_entry = user.clone();
             Ok(user)
         } else {
-            Err(DatabaseError::UserNotFound)
+            Err(DatabaseError::NotFoundError)
         }
     }
 
@@ -89,7 +92,113 @@ impl UserRepository for InMemoryUserRepository {
         if data.remove(id).is_some() {
             Ok(())
         } else {
-            Err(DatabaseError::UserNotFound)
+            Err(DatabaseError::NotFoundError)
+        }
+    }
+}
+
+
+
+#[derive(Debug, Clone)]
+pub struct InMemoryGroupRepository {
+    data: Arc<RwLock<HashMap<String, Arc<RwLock<Group>>>>>,
+}
+
+impl InMemoryGroupRepository {
+    pub fn new() -> Self {
+        InMemoryGroupRepository {
+            data: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
+}
+
+impl GroupRepository for InMemoryGroupRepository {
+    fn get_group_by_id(&self, id: &str) -> Result<Group, DatabaseError> {
+        // Acquire read lock on the main HashMap
+        let data = self.data.read().map_err(|_| DatabaseError::ReadLockError)?;
+        
+        // Acquire read lock on the specific Group (if it exists)
+        if let Some(group_lock) = data.get(id) {
+            let group = group_lock.read().map_err(|_| DatabaseError::ReadLockError)?;
+            Ok(group.clone())
+        } else {
+            Err(DatabaseError::NotFoundError)
+        }
+    }
+
+    fn get_group_by_name(&self, name: &str) -> Result<Group, DatabaseError> {
+        // Acquire read lock on the main HashMap
+        let data = self.data.read().map_err(|_| DatabaseError::ReadLockError)?;
+
+        // Iterate over all entries and check by name
+        for group_lock in data.values() {
+            let group = group_lock.read().map_err(|_| DatabaseError::ReadLockError)?;
+            if group.name.value() == name {
+                return Ok(group.clone());
+            }
+        }
+        Err(DatabaseError::NotFoundError)
+    }
+
+    fn get_group_by_description(&self, description: &str) -> Result<Group, DatabaseError> {
+        // Acquire read lock on the main HashMap
+        let data = self.data.read().map_err(|_| DatabaseError::ReadLockError)?;
+
+        // Iterate over all entries and check by description
+        for group_lock in data.values() {
+            let group = group_lock.read().map_err(|_| DatabaseError::ReadLockError)?;
+            if group.description.value() == description {
+                return Ok(group.clone());
+            }
+        }
+        Err(DatabaseError::NotFoundError)
+    }
+
+    fn get_all_groups(&self) -> Result<Vec<Group>, DatabaseError> {
+        // Acquire read lock on the main HashMap
+        let data = self.data.read().map_err(|_| DatabaseError::ReadLockError)?;
+
+        // Iterate over all entries and clone them
+        Ok(data.values().map(|group_lock| {
+            group_lock.read().map(|group| group.clone()).unwrap()
+        }).collect())
+    }
+    
+    fn create_group(&self, group: Group) -> Result<Group, DatabaseError> {
+        // Acquire write lock on the main HashMap to insert a new group
+        let mut data = self.data.write().map_err(|_| DatabaseError::WriteLockError)?;
+
+        if data.contains_key(&group.id.value().to_string()) {
+            return Err(DatabaseError::RegisterAlreadyExists);
+        }
+
+        // Insert the new group with an Arc<RwLock<Group>>
+        data.insert(group.id.value().to_string().clone(), Arc::new(RwLock::new(group.clone())));
+        Ok(group)
+    }
+
+    fn update_group(&self, group: Group) -> Result<Group, DatabaseError> {
+        // Acquire read lock on the main HashMap to find the specific group
+        let data = self.data.read().map_err(|_| DatabaseError::ReadLockError)?;
+
+        if let Some(group_lock) = data.get(group.id.value()) {
+            // Acquire write lock on the specific Group entry to modify it
+            let mut group_entry = group_lock.write().map_err(|_| DatabaseError::WriteLockError)?;
+            *group_entry = group.clone();
+            Ok(group)
+        } else {
+            Err(DatabaseError::NotFoundError)
+        }
+    }
+
+    fn delete_group(&self, id: &str) -> Result<(), DatabaseError> {
+        // Acquire write lock on the main HashMap to remove the group
+        let mut data = self.data.write().map_err(|_| DatabaseError::WriteLockError)?;
+
+        if data.remove(id).is_some() {
+            Ok(())
+        } else {
+            Err(DatabaseError::NotFoundError)
         }
     }
 }
